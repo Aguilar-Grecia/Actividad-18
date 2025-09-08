6a3ee178aedadb3adfcbda140cfe75b60e6812a1
Proyectos de Pycharm <git@github.com:Aguilar-Grecia/PycharmProjects.git>
2025-09-08 00:46:28 -0600

    commit

--- Concurso de bandas.py
+++ Concurso de bandas.py
@@ -86,45 +86,46 @@ class ConcursoBanda:
         self.ventana = ventana
         self.ventana.title("Concurso de bandas")
         self.ventana.geometry("900x500")
-        ventana.configure(bg= "Beige")
+        ventana.configure(bg= "#595775")
 
-        etiqueta = tk.Label(ventana, text="SISTEMA DE CONCURSO DE BANDAS", font=("georgia", 35, "underline"), fg="goldenrod", bg="beige")
+        etiqueta = tk.Label(ventana, text="SISTEMA DE CONCURSO DE BANDAS", font=("georgia", 35, "underline"), fg="#F1E0D6", bg="#595775")
         etiqueta.pack(pady=5)
         self.concurso = Concurso("Concurso de bandas - 14 de Septiembre", "2025-09-14")
 
-        boton_inscribir_banda = tk.Button(self.ventana, text="Inscribir Banda", command=self.inscribir_banda,font=("georgia", 13, "italic"), fg="dark slate gray", bg= "beige")
+        boton_inscribir_banda = tk.Button(self.ventana, text="Inscribir Banda", command=self.inscribir_banda,font=("georgia", 15, "italic"), fg="#F1E0D6", bg= "#583E2E", bd=5)
         boton_inscribir_banda.pack(pady=5)
 
-        boton_registrar_evaluacion = tk.Button(self.ventana, text="Registrar Evaluacion", command=self.registrar_evaluacion,font=("georgia", 13, "italic"), fg="dark slate gray", bg= "beige")
+        boton_registrar_evaluacion = tk.Button(self.ventana, text="Registrar Evaluacion", command=self.registrar_evaluacion,font=("georgia", 15, "italic"), fg="#F1E0D6", bg= "#583E2E", bd=5)
         boton_registrar_evaluacion.pack(pady=5)
 
-        boton_listar_bandas = tk.Button(self.ventana, text="Listar Bandas", command=self.listar_bandas,font=("georgia", 13, "italic"), fg="dark slate gray", bg= "beige")
+        boton_listar_bandas = tk.Button(self.ventana, text="Listar Bandas", command=self.listar_bandas,font=("georgia", 15, "italic"), fg="#F1E0D6", bg= "#583E2E", bd=5)
         boton_listar_bandas.pack(pady=5)
 
-        boton_ver_ranking = tk.Button(self.ventana, text="Ver Ranking", command=self.ver_ranking,font=("georgia", 13, "italic"), fg="dark slate gray", bg= "beige")
+        boton_ver_ranking = tk.Button(self.ventana, text="Ver Ranking", command=self.ver_ranking,font=("georgia", 15, "italic"), fg="#F1E0D6", bg= "#583E2E", bd=5)
         boton_ver_ranking.pack(pady=5)
 
-        boton_guardar = tk.Button(self.ventana, text="Guardar Resultados", command=self.guardar_archivo,font=("georgia", 13, "italic"), fg="dark slate gray", bg= "beige")
+        boton_guardar = tk.Button(self.ventana, text="Guardar Resultados", command=self.guardar_archivo,font=("georgia", 15, "italic"), fg="#F1E0D6", bg= "#583E2E", bd=5)
         boton_guardar.pack(pady=5)
 
-        boton_salir = tk.Button(self.ventana, text="Salir", command=self.ventana.quit, font=("georgia", 13, "italic"), fg="dark slate gray", bg= "beige")
+        boton_salir = tk.Button(self.ventana, text="Salir", command=self.ventana.quit, font=("georgia", 15, "italic"), fg="#F1E0D6", bg= "#583E2E", bd=5)
         boton_salir.pack(pady=5)
 
     def inscribir_banda(self):
         ventana_form = tk.Toplevel(self.ventana)
         ventana_form.title("Inscribir Banda")
-        ventana_form.geometry("600x500")
+        ventana_form.geometry("800x600")
+        ventana_form.configure(bg="#2A3457")
 
-        tk.Label(ventana_form, text="Nombre de la Banda:").pack(pady=5)
-        entry_nombre = tk.Entry(ventana_form)
+        tk.Label(ventana_form, text="Nombre de la Banda:",font=("georgia", 13, "italic"), fg= "#F2EAED",bg= "#2A3457" ).pack(pady=5)
+        entry_nombre = tk.Entry(ventana_form, fg= "#2A3457",bg= "#A4A4BF", bd=5, font=("georgia", 12, "italic"))
         entry_nombre.pack(pady=5)
 
-        tk.Label(ventana_form, text="Institución:").pack(pady=5)
-        entry_institucion = tk.Entry(ventana_form)
+        tk.Label(ventana_form, text="Institución:",font=("georgia", 13, "italic"), fg= "#F2EAED",bg= "#2A3457").pack(pady=5)
+        entry_institucion = tk.Entry(ventana_form,fg= "#2A3457",bg= "#A4A4BF", bd=5, font=("georgia", 12, "italic"))
         entry_institucion.pack(pady=5)
 
-        tk.Label(ventana_form, text="Categoria (Primaria/Básico/Diversificado):").pack(pady=5)
-        entry_categoria = tk.Entry(ventana_form)
+        tk.Label(ventana_form, text="Categoria (Primaria/Básico/Diversificado):",font=("georgia", 13, "italic"), fg= "#F2EAED",bg= "#2A3457").pack(pady=5)
+        entry_categoria = tk.Entry(ventana_form,fg= "#2A3457",bg= "#A4A4BF", bd=5, font=("georgia", 12, "italic"))
         entry_categoria.pack(pady=5)
 
         def guardar():
@@ -133,34 +134,36 @@ class ConcursoBanda:
             categoria = entry_categoria.get().strip()
 
             if not nombre or not institucion or not categoria:
-                tk.Label(ventana_form, text="Todos los campos son obligatorios").pack(pady=5)
+                tk.Label(ventana_form, text="Todos los campos son obligatorios", fg= "#2A3457",bg= "#A4A4BF").pack(pady=5)
                 return
 
             banda = BandaEscolar(nombre, institucion, categoria)
             ok, mensaje = self.concurso.inscribir_banda(banda)
             tk.Label(ventana_form, text=mensaje).pack(pady=5)
 
-        tk.Button(ventana_form, text="Guardar", command=guardar).pack(pady=10)
+        tk.Button(ventana_form, text="Guardar", command=guardar,font=("georgia", 15, "italic"), fg= "#F2EAED",bg= "#888C46", bd=8).pack(pady=10)
 
     def registrar_evaluacion(self):
         ventana_evalua = tk.Toplevel(self.ventana)
         ventana_evalua.title("Registrar Evaluación")
-        ventana_evalua.geometry("600x500")
+        ventana_evalua.geometry("900x600")
+        ventana_evalua.configure(bg="#B5C1B4")
+
 
         nombres_bandas = list(self.concurso.bandas.keys())
         if not nombres_bandas:
-            tk.Label(ventana_evalua, text="No hay bandas registradas para evaluar").pack(pady=5)
+            tk.Label(ventana_evalua, text="No hay bandas registradas para evaluar",font=("georgia", 15, "italic"), fg= "#3F3232",bg= "#B5C1B4").pack(pady=5)
             return
 
-        tk.Label(ventana_evalua, text="Seleccione la banda a Evaluar:").pack(pady=5)
+        tk.Label(ventana_evalua, text="Seleccione la banda a Evaluar:",font=("georgia", 15, "italic"), fg= "#3F3232",bg= "#B5C1B4").pack(pady=5)
         seleccion = tk.StringVar()
         seleccion.set(nombres_bandas[0])
         tk.OptionMenu(ventana_evalua, seleccion, *nombres_bandas).pack(pady=5)
 
         entradas = {}
         for criterio in BandaEscolar._criterios_evaluacion:
-            tk.Label(ventana_evalua, text=f"{criterio} (0-10):").pack(pady=5)
-            entry = tk.Entry(ventana_evalua)
+            tk.Label(ventana_evalua, text=f"{criterio} (0-10):",font=("georgia", 15, "italic"), fg= "#3F3232",bg= "#B5C1B4").pack(pady=5)
+            entry = tk.Entry(ventana_evalua,fg= "#1B1924",bg= "#DCD9C6", bd=5, font=("georgia", 12, "italic"))
             entry.pack(pady=5)
             entradas[criterio] = entry
 
@@ -183,37 +186,38 @@ class ConcursoBanda:
 
             if ok:
                 ventana_evalua.destroy()
-        tk.Button(ventana_evalua, text="Guardar Evaluación", command=guardar_evalua).pack(pady=10)
+        tk.Button(ventana_evalua, text="Guardar Evaluación", command=guardar_evalua,fg= "#DCD9C6",bg= "#74593D", bd=5, font=("georgia", 14, "italic")).pack(pady=10)
 
     def listar_bandas(self):
         ventana_lista = tk.Toplevel(self.ventana)
         ventana_lista.title("Listado de Bandas")
         ventana_lista.geometry("600x500")
+        ventana_lista.configure(bg="#F2C083")
 
         bandas = self.concurso.listar_bandas()
         if not bandas:
-            tk.Label(ventana_lista, text="No hay bandas registradas").pack(pady=5)
-        else:
-            for info in bandas:
-                tk.Label(ventana_lista, text=info).pack(pady=5)
+            tk.Label(ventana_lista, text="No hay bandas registradas", font=("georgia", 15, "italic"), fg= "#582A20",bg= "#F2C083").pack(pady=5)
+        for info in bandas:
+            tk.Label(ventana_lista, text=info, font=("georgia", 15, "italic"), fg= "#9499A6",bg= "#582A20").pack(pady=5)
 
     def ver_ranking(self):
         ventana_ranking = tk.Toplevel(self.ventana)
         ventana_ranking.title("Clasificación Final")
-        ventana_ranking.geometry("600x500")
+        ventana_ranking.geometry("900x700")
+        ventana_ranking.configure(bg="#525B56")
 
         ranking = self.concurso.ranking()
         if not ranking:
-            tk.Label(ventana_ranking, text="No hay evaluaciones registradas").pack(pady=5)
+            tk.Label(ventana_ranking, text="No hay evaluaciones registradas",font=("georgia", 17, "italic"), fg= "#BE9063",bg= "#525B56").pack(pady=5)
         else:
             for i, banda in enumerate(ranking, start=1):
-                tk.Label(ventana_ranking, text=f"{i}. {banda.nombre} ({banda.institucion}) - {banda.total()} puntos").pack(pady=5)
+                tk.Label(ventana_ranking, text=f"{i}. {banda.nombre} ({banda.institucion}) - {banda.total()} puntos",font=("georgia", 18, "italic"), fg= "#A4978E",bg= "#132226").pack(pady=5)
 
     def guardar_archivo(self):
         mensaje = self.concurso.guardar_resultados()
         ventana_msg = tk.Toplevel(self.ventana)
         ventana_msg.title("Guardar Archivo")
-        tk.Label(ventana_msg, text=mensaje).pack(pady=10)
+        tk.Label(ventana_msg, text=mensaje, font=("georgia", 18, "italic"), fg= "#D9AC2A",bg= "#270101").pack(pady=10)
 
 if __name__ == "__main__":
     root = tk.Tk()
